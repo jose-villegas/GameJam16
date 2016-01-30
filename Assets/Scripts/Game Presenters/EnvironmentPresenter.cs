f318b4248c33c5639a2a96d4ba093d063e94082f
Plnero <nero_lucho92@hotmail.com>
2016-01-30 12:34:21 -0430

    Spawn system desu ogoroshimas baka u baka

---
+++ Assets/Scripts/Game Presenters/EnvironmentPresenter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentPresenter : MonoBehaviour
+{
+    private RoomPresenter[] rooms = new RoomPresenter[0];
+
+	// Use this for initialization
+	public void Initialize () {
+
+        // Get all rooms references
+	    this.rooms = this.GetComponentsInChildren<RoomPresenter>();
+
+        // Initialize all rooms
+	    foreach (var room in this.rooms)
+	    {
+	        room.Initialize();
+	    }	
+	}
+
+    // Returns a random spawn location in a random room
+    public Vector3 GetValidPlayerSpawnPosition()
+    {
+        if (this.rooms.Length <= 0)
+            return Vector3.zero;
+        else
+        {
+            return this.rooms[Random.Range(0, this.rooms.Length - 1)].GetRandomSpawnLocation();
+        }
+    }
+}
