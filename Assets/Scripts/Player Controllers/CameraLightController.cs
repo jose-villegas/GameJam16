9e9c6febd48977cfe27f3932390b9813a6c17115
Plnero <nero_lucho92@hotmail.com>
2016-01-30 17:40:13 -0430

    Lighto System

--- Assets/Scripts/Player Controllers/CameraLightController.cs
+++ Assets/Scripts/Player Controllers/CameraLightController.cs
@@ -1,15 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Camera))]
 public class CameraLightController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Component references
+    private PlayerController mainPlayerController;
+
+    public void Initialize(PlayerController playerController)
+    {
+        // Store references
+        this.mainPlayerController = playerController;
+    }
+    
+    void OnPreCull()
+    {
+        if (this.mainPlayerController == null)
+            return;
+
+        switch (this.mainPlayerController.Type)
+        {
+            case Entity.PlayerType.Human:
+                break;
+            case Entity.PlayerType.Monster:
+                // If monster, disable human lights
+                foreach (var light in GamePresenter.Instance.EnvironmentPresenter.HumanLights)
+                    light.enabled = false;
+                break;
+        }
+
+
+    }
+
+    void OnPostRender()
+    {
+        // Reenable disabled lights
+        foreach (var light in GamePresenter.Instance.EnvironmentPresenter.HumanLights)
+        {
+            light.enabled = true;
+        }
+    }
 }
