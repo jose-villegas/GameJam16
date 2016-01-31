07545a41b9b0d250adc6048fcd3a1a82472375ba
Plnero <nero_lucho92@hotmail.com>
2016-01-31 01:54:15 -0430

    Noise Visual VFX

--- Assets/Scripts/Player Controllers/CameraLightController.cs
+++ Assets/Scripts/Player Controllers/CameraLightController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.ImageEffects;
 
 [RequireComponent(typeof(Camera))]
 public class CameraLightController : MonoBehaviour {
@@ -8,12 +9,63 @@ public class CameraLightController : MonoBehaviour {
     // Component references
     private PlayerController mainPlayerController;
 
+    // Noise effect
+    public NoiseAndGrain NoiseVFX;
+    [Range(0,10)]
+    public float MaxNoise = 9.0f;
+
+    public float MaxNoiseRange = 15.0f;
+    public float MinNoiseRange = 3.0f;
+
     public void Initialize(PlayerController playerController)
     {
         // Store references
         this.mainPlayerController = playerController;
     }
-    
+
+    // Set special vfxs
+    void Update()
+    {
+        switch (this.mainPlayerController.Type)
+        {
+            case Entity.PlayerType.Human:
+                this.ManageNoiseVFX();
+                break;
+            case Entity.PlayerType.Monster:
+                this.ManageNoiseVFX(false);
+                break;
+        }
+    }
+
+    // Manage noise vfx
+    private void ManageNoiseVFX(bool useVFX = true)
+    {
+        this.NoiseVFX.enabled = useVFX;
+
+        // Manage noise intensity
+        if (this.NoiseVFX.enabled)
+        {
+            // Get closest player
+            PlayerController closestMonsterController =
+                GamePresenter.Instance.PlayerPresenter.GetClosestMonsterPlayer(this.mainPlayerController);
+
+            // If there is no close player, disable the effect
+            if (closestMonsterController == null)
+            {
+                this.NoiseVFX.intensityMultiplier = 0;
+                return;
+            }
+
+            // Check if the distance is within the target distance
+            float currentDistance = Vector3.Distance(this.mainPlayerController.transform.position,
+                closestMonsterController.transform.position);
+
+            // Set final noise intensity
+            this.NoiseVFX.intensityMultiplier = this.MaxNoise * Mathf.Lerp(1,0, currentDistance / MaxNoiseRange);
+        }
+
+    }
+
     void OnPreCull()
     {
         if (this.mainPlayerController == null)
@@ -29,8 +81,6 @@ public class CameraLightController : MonoBehaviour {
                     light.enabled = false;
                 break;
         }
-
-
     }
 
     void OnPostRender()
