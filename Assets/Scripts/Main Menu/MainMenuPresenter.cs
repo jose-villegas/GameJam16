b26a8ab71e3cef56c6bc3965ebf19e72e2b6c622
Plnero <nero_lucho92@hotmail.com>
2016-01-31 09:03:54 -0430

    ight

--- Assets/Scripts/Main Menu/MainMenuPresenter.cs
+++ Assets/Scripts/Main Menu/MainMenuPresenter.cs
@@ -2,14 +2,43 @@
 using System.Collections;
 
 public class MainMenuPresenter : MonoBehaviour {
+    // UI Panels
+    public CanvasGroup IntroPanel;
+    public CanvasGroup MainMenuPanel;
+    public CanvasGroup RulesPanel;
+    public CanvasGroup HistoryPanel;
+    public CanvasGroup CreditsPanel;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+	    // Go to intro panel
+        this.GoToIntroPanel();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Go Tos
+    public void GoToIntroPanel()
+    {
+        
+    }
+
+    public void GoToMainMenuPanel()
+    {
+        
+    }
+
+    public void GoToRulesPanel()
+    {
+        
+    }
+
+    public void GoToHistoryPanel()
+    {
+        
+    }
+
+    public void GoToCredits()
+    {
+        
+    }
+
 }
